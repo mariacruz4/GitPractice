1f3faa250d92bdde6b7a2146b14220489cca550b
Mariacruz4 <eloisa943@gmail.com>
2019-07-16 09:26:54 -0500

    Print lines

---
+++ GitPractice/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello.");
+        System.out.println("Random print lines.");
+        System.out.println("More random print lines.");
+    }
+}
